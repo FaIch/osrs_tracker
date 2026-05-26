ae695ab85abe49f47a2b6291b87aaba7d3340b20
Joakim Falch <joakimfalch@outlook.com>
2026-05-26 19:38:54 +0200

    added boss icons

--- src/config.ts
+++ src/config.ts
@@ -8,4 +8,3 @@ export const PLAYERS = [
 export type PlayerName = (typeof PLAYERS)[number];
 
 export const WOM_API = 'https://api.wiseoldman.net/v2';
-export const WOM_CDN = 'https://cdn.wiseoldman.net/images/metrics';
